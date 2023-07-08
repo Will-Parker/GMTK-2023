12d8acad1fb05672ae23942c5af58210aa8aee5a
sonicpokemonguy <WilliamParkerProfessionalEmail@gmail.com>
2023-07-07 20:03:35 -0400

    organize cards

--- Assets/Scripts/New Folder/CardCollection.cs
+++ Assets/Scripts/New Folder/CardCollection.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class CardCollection : MonoBehaviour
 {
-    [SerializeField]
-    private List<Card> cards;
+    private const float card_width = 0.2f;
+    [SerializeField] private List<Card> cards;
     // Start is called before the first frame update
     void Start()
     {
         cards.AddRange(transform.GetComponentsInChildren<Card>(false));
+        OrganizeCards();
     }
 
     // Update is called once per frame
@@ -23,6 +24,11 @@ public class CardCollection : MonoBehaviour
 
     private void OrganizeCards()
     {
-        
+        float collectionWidth = cards.Count * card_width;
+        float startPosition = (collectionWidth / (-2)) + (card_width / 2);
+        for (int i = 0; i < cards.Count; i++)
+        {
+            cards[i].transform.localPosition = Vector3.right * (startPosition + (i * card_width));
+        }
     }
 }
